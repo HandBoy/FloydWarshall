4c2cd0d756e2be18c4532e8796c4ce50388b2e39
handerson <handersonmedeiros@gmail.com>
2016-11-30 17:46:27 -0300

    implementando algoritmo do floyd

--- FloydWarshall/src/MainFloyd.java
+++ FloydWarshall/src/MainFloyd.java
@@ -1,4 +1,5 @@
 //http://www.programming-algorithms.net/article/45708/Floyd-Warshall-algorithm
+//http://www.geeksforgeeks.org/dynamic-programming-set-16-floyd-warshall-algorithm/
 
 import java.io.File;
 
@@ -9,48 +10,39 @@ import util.Reader;
 public class MainFloyd {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		if(args.length == 0){
-			System.out.println("Passe algum argumento para o programa");
-			System.exit(0);
-		}
+		int [][] matriz = new int[5][5];
+		int [][][] D = new int[6][5][5];
 		File arquivo = new File(args[0]);
-		
-		if(arquivo.exists()){
-			System.out.println("Arquivo Encontrado.");
-			int [][] matriz = Reader.lerArquivo(arquivo);
-			floydWarshall(matriz);
-			/*for (int i = 0; i < matriz.length; i++) {
-				for (int j = 0; j < matriz.length; j++) {
-					System.out.printf("%3d",  matriz[i][j]);
-				}
-				System.out.print("\n");
-			}*/
-		} else {
-			System.out.println("Arquivo Não Encontrado: " + args[0]);
-			System.exit(0);
-		}
-
-	}
-	
-	public static void floydWarshall(int[][] matriz){
-		for (int k = 0; k < matriz.length; k++) {
-			for (int i = 0; i < matriz.length; i++) {
-				for (int j = 0; j < matriz.length; j++) {
-					matriz[i][j] = Integer.min(matriz[i][j],matriz[i][k]+matriz[k][j]);
-				}
-			}
-		}
+		matriz = Reader.lerArquivo(arquivo);
+		int cont = 0;
+		int x = 0;
 		
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz.length; j++) {
-				System.out.printf("%4d",  matriz[i][j]);
+				D[0][i][j] = matriz[i][j];
+			}
+		}				
+		
+		for (int k = 1; k < D.length; k++) {
+			for (int i = 0; i < matriz.length; i++) {
+				for (int j = 0; j < matriz.length; j++) {
+					cont = Integer.min(D[k-1][i][j], D[k-1][i][x]+D[k-1][x][j]);
+					D[k][i][j] = cont;
+				}				
+			}
+			x++;				
+		}
+		System.out.println("=======================");
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
+				if(D[0][i][j] > 999)
+					System.out.printf("   INF");
+				else
+					System.out.printf("%6d",  D[3][i][j]);
 			}
 			System.out.print("\n");
 		}
-		
+
 	}
-	
-	
 
 }
